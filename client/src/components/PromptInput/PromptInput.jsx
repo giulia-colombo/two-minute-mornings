0ef4ac4147d363f060a07c8f744d05792e3a27be
giulia-colombo <giulia.colombo.127@gmail.com>
2024-01-31 18:10:39 +0100

    feat(PromptInput): add Bootstrap to component

--- client/src/components/PromptInput/PromptInput.jsx
+++ client/src/components/PromptInput/PromptInput.jsx
@@ -1,5 +1,8 @@
 import { useState } from 'react';
 import entryService from '../../services/entry.service';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import Container from 'react-bootstrap/esm/Container';
 
 const PromptInput = () => {
   // Setting up state variables and setter functions
@@ -42,40 +45,93 @@ const PromptInput = () => {
   };
 
   return (
-    <div className="PromptInput">
-      <form onSubmit={handleSubmit}>
-        <label>Today I will focus on...</label>
-        <textarea
-          type="text"
-          name="focusPrompt"
-          value={focusPrompt} //the value attribute connects the input to the state
-          onChange={handleFocusPrompt}
-          autoFocus
-        />
+    <Container>
+      <Form onSubmit={handleSubmit}>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Today I will focus on:</Form.Label>
+          <Form.Control
+            type="text"
+            name="focusPrompt"
+            value={focusPrompt} //the value attribute connects the input to the state
+            onChange={handleFocusPrompt}
+            autoFocus
+            placeholder="Your thoughts here..."
+          />
+        </Form.Group>
 
-        <label>Today I am grateful for...</label>
-        <textarea
-          type="text"
-          name="gratefulPrompt"
-          value={gratefulPrompt} //the value attribute connects the input to the state
-          onChange={handleGratefulPrompt}
-        />
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Today I am grateful for:</Form.Label>
+          <Form.Control
+            type="text"
+            name="focusPrompt"
+            value={gratefulPrompt} //the value attribute connects the input to the state
+            onChange={handleGratefulPrompt}
+            placeholder="Your thoughts here..."
+          />
+        </Form.Group>
 
-        <label>Today I will let go of...</label>
-        <textarea
-          type="text"
-          name="letGoPrompt"
-          value={letGoPrompt} //the value attribute connects the input to the state
-          onChange={handleLetGoPrompt}
-        />
-        <button type="submit">Save your entry</button>
-      </form>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Today I let go of:</Form.Label>
+          <Form.Control
+            type="text"
+            name="letGoPrompt"
+            value={letGoPrompt} //the value attribute connects the input to the state
+            onChange={handleLetGoPrompt}
+            placeholder="Your thoughts here..."
+          />
+        </Form.Group>
+
+        <Button variant="primary" type="submit">
+          Save your entry
+        </Button>
+      </Form>
 
       {successMessage && <div className="successMessage">{successMessage}</div>}
 
       {error && <div className="errorMessage">{error}</div>}
-    </div>
+    </Container>
   );
 };
 
+//   return (
+//     <div className="PromptInput">
+//       <form onSubmit={handleSubmit}>
+//         <label>Today I will focus on...</label>
+//         <textarea
+//           type="text"
+//           name="focusPrompt"
+//           value={focusPrompt} //the value attribute connects the input to the state
+//           onChange={handleFocusPrompt}
+//           autoFocus
+//         />
+
+//         <label>Today I am grateful for...</label>
+//         <textarea
+//           type="text"
+//           name="gratefulPrompt"
+//           value={gratefulPrompt} //the value attribute connects the input to the state
+//           onChange={handleGratefulPrompt}
+//         />
+
+//         <label>Today I will let go of...</label>
+//         <textarea
+//           type="text"
+//           name="letGoPrompt"
+//           value={letGoPrompt} //the value attribute connects the input to the state
+//           onChange={handleLetGoPrompt}
+//         />
+//         <button type="submit">Save your entry</button>
+//       </form>
+
+//       {successMessage && <div className="successMessage">{successMessage}</div>}
+
+//       {error && <div className="errorMessage">{error}</div>}
+//     </div>
+//   );
+// };
+
 export default PromptInput;
+
+// function BasicExample() {
+
+// export default BasicExample;
